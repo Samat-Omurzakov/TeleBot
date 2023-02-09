cf0bf2aa825eb0aaca73b95bc4c45687650c89a0
Samat-Omurzakov <samat-omurzakov@mail.ru>
2023-02-09 20:06:29 +0600

    lesson2

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ import logging
 import decouple
 from decouple import config
 
-
 TOKEN = config('TOKEN')
 
 bot = Bot(TOKEN)
 db = Dispatcher(bot=bot)
 
+
+@db.message_handler(commands=['start', 'hello'])
+async def start_handler(message: types.Message):
+    await bot.send_message(message.from_user.id, f'Hi {message.from_user.first_name}')
+    await message.answer('Its answer')
+    await message.reply('Its reply')
+
+
+@db.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.from_user.id, message.text)
+    await message.answer('echo')
 
 
 if __name__ == '__main__':
